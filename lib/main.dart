e4bd3aa289441dcb78da979bfaa6b16086a28f6b
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-10-05 15:36:58 +0100

    Rewriting in dart

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Clean Code',
+      debugShowMaterialGrid: false,
+      theme: ThemeData(
+        fontFamily: "Nunito",
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
